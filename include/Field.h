90f21af597cb794eb23b8deaadf97b5f5d450863
Ric Rodriguez <therickyross2@gmail.com>
2018-10-21 13:44:18 -0700

    Refactored for inlining trivial functions
    Signed-off-by: therickyross2 <therickyross2@gmail.com>

--- include/Field.h
+++ include/Field.h
@@ -1,32 +1,31 @@
+#ifndef FIELD_H
+#define FIELD_H
+
 #include "Potentials.h"
-
-#ifndef Field_H
-#define Field_H
-
 
 class Field {
 
-	private:
-		double x,y;
-		double abs;
+ private:
+  double x = 0;
+  double y = 0;
+  double abs = 0;
 
-	public:
-		Field();
-		virtual ~Field();
-		double GetFieldx();
-		double GetFieldy();
-		double Getabs();
-		void Setabs(double);
-		void SetField(double,double);
-		void AddField(double,double);
+ public:
+  Field();
+  inline virtual ~Field() = default;
+  inline double GetFieldx() { return x; };
+  inline double GetFieldy() { return y; };
+  inline double Getabs() { return abs; };
+  inline void Setabs(double absValue) { abs = absValue; };
+  inline void SetField(double xValue, double yValue) { x = xValue, y = yValue; }
+  inline void AddField(double xValue, double yValue) { x += xValue, y += yValue; }
 
-		ClassDef(Field,0); 
+ ClassDef(Field, 0);
 };
 
+void CalculateFields(Potentials &, Field **, Field **);
+void CalculateAbsFields(Potentials &, Field **, Field **);
+//Field** RotateField(Potentials &,Field**,double);
+void RotateField(Potentials &, Field **, Field **, double);
 
 #endif
-
-void CalculateFields(Potentials&,Field**,Field**);
-void CalculateAbsFields(Potentials&,Field**,Field**);
-//Field** RotateField(Potentials &,Field**,double);
-void RotateField(Potentials &,Field**,Field**,double);
